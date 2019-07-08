ff4aa6951bee372f7d16fb131b5f46ea62d89130
Abishek0123 <52314383+Abishek0123@users.noreply.github.com>
2019-07-08 10:57:00 +0530

    Update larger.py

--- larger.py
+++ larger.py
@@ -3,7 +3,7 @@ num2 = float(input(" "))
 num3 = float(input(" "))
  
 if (num1 > num2) and (num1 > num3):
-   large = num1
+   largest = num1
 elif (num2 > num1) and (num2 > num3):
    largest = num2
 else:
