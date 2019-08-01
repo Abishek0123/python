6e496be402ae9b74929d5caf95415aac9ab4af06
Abishek0123 <52314383+Abishek0123@users.noreply.github.com>
2019-08-01 11:25:04 +0530

    Update positive.py

--- positive.py
+++ positive.py
@@ -1,7 +1,7 @@
-n=int(input("enter the number:"))
+n=int(input(" "))
 if n>0:
     print("positive")
+elif n<0:
+    print("negative")
 elif n==0:
     print("zero")
-elif n<0:
-    print("invalid")
